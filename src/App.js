903e82ee2f58dc9f411e6baec378f1bf32632c7b
SINGH 1258 <sumantpaldm@gmail.com>
2021-11-07 14:30:50 -0500

    added hooks to components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import Gallery from "./components/Gallery";
+import Gallery from './components/Gallery';
 
 function App() {
-
   const [categories] = useState([
     {
       name: 'commercial',
@@ -25,12 +24,11 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
 }
+
 export default App;
